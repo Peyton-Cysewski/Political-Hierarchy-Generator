8c4c0f0f16106bfaee216138ec4a0ac4cecc7e17
Peng Chen <pchen0816@outlook.com>
2020-04-28 11:47:12 -0700

    periodic save

--- server.js
+++ server.js
@@ -29,25 +29,27 @@ dbClient.connect(error => {
 
 // ALL ROUTES + CALLBACKS
 // **NOTE** - Callbacks need to be defined in module directories and imported directly above this section.
-// ROUTE TYPE -----------------------------------------   STATUS   ---------------   DESCRIPTION (if necessary)   ----------------------------------------------- //
+// ROUTE TYPE -----------------------------------------   STATUS   ---------------   DESCRIPTION   --------------------------------------------------------- //
 // GET
-app.get('/', renderHomePage);                           // TODO // - will display different view when user is logged out versus when logged in.
-app.get('/hierarchy', renderHierarchy);                 // TODO // - will display different view when user is logged out versus when logged in.
-app.get('/login', renderLoginPage);                     // TODO //
-app.get('/createAccount', renderNewAccount);            // TODO //
-app.get('/saved', renderSavedHierarchies);              // TODO //
-app.get('/paginatedSaved', renderPaginatedHierarchies); // TODO //
+app.get('/', renderHomePage);                           // TODO // - will display different header view when user is logged out versus when logged in.
+app.get('/hierarchy', renderHierarchy);                 // TODO // - will display different header view when user is logged out versus when logged in.
+app.get('/login', renderLoginPage);                     // TODO // - displays the login page with form for username and password; a link to create account is also visible.
+app.get('/createAccount', renderNewAccount);            // TODO // - displays a similar form to the login page; (Stretch: as the user puts in their username, checks database and denies it if it has already been taken, case sensitive).
+app.get('/saved', renderSavedHierarchies);              // TODO // - displays a list of all the save-names of the hierarchies that user has ever created; (Stretch: can sort by name or by hidden id).
+app.get('/paginatedSaved', renderPaginatedHierarchies); // TODO // - displays a hierarchy in the same format that the 'renderHierarchy' function shows, but with paging buttons that paginate through the list from the previous view.
 // Stretch
 // app.get('/accountSettings', renderAccountSettings);  // TODO //
 // app.get('/about', renderAboutPage);                  // TODO //
 
 // POST
 app.post('/input', createHierarchy);                    // TODO // - generates hierarchy before rendering it.
-app.post('/login', login);                              // TODO // - logs the user in, returning them to the page they were just at, either to the home page or the hierarchy they just created while logged out.
+app.post('/login', login);                              // TODO // - logs the user in by checking to see if their username and password match, returning them to the page they were just at, either to the home page or the hierarchy they just created while logged out.
 
 // PUT
 app.put('/createAccount', saveUser);                    // TODO // - sends login data to be saved to database 'project' in table 'users'. 
 app.put('/save', saveHierarchy);                        // TODO // - saves the proper hierarchy metadata to the database 'project' in tables 'politics' and 'hierarchy'.
+// Stretch
+// app.put('/updateUser', updateUser)                   // TODO //
 
 
 
